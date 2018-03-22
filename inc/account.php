2f4391ff30633abc133085f1203445524e5d0d44
Erwin <erwinhayag85@gmail.com>
2018-03-22 15:04:25 +0800

    Fix Customer Settings

--- inc/account.php
+++ inc/account.php
@@ -10,24 +10,24 @@
 	  <div class="form-group">
 
 	    <label>First Name</label>
-	    <input type="text" class="form-control">		
+	    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['userInfo']['UserFirstName'] ?>" disabled>	
 	  </div>
 	  <div class="form-group">
 	    <label>Last Name</label>
-	    <input type="text" class="form-control" >
+	    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['userInfo']['UserLastName'] ?>" disabled>
 	  </div>
 	   <div class="form-group">
 	    <label>Email Address</label>
-	    <input type="text" class="form-control">
+	    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['userInfo']['UserEmail'] ?>" disabled>
 	  </div>
 	  <div class="form-group">
 	    <label>Change Password</label>
-	    <input type="password" class="form-control">
+	    <input type="password" class="form-control" disabled>
 	  </div>
 
 	  <div class="form-group">
 	    <label>Phone Number</label>
-	    <input type="text" class="form-control">
+	    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['userInfo']['UserPhone'] ?>" disabled>
 	  </div>
 	 
 	 </div>
@@ -35,7 +35,7 @@
 	  <div class="form-group">
 	    <label>City</label>
 	    <select class="form-control">
-	    	<option>Choose City</option>
+	    	<option><?php $city = $_SESSION['userInfo']['UserCity']==""? "Choose City":$_SESSION['userInfo']['UserCity']; echo $city; ?></option>
 	     	<option>Philippines</option>
 	     	<option>Singapore</option>
 	      <option>Korea</option>
@@ -44,7 +44,7 @@
   	<div class="form-group">
 	    <label>State</label>
 	    <select class="form-control">
-	    	<option>Choose State</option>
+	    	<option><?php $state = $_SESSION['userInfo']['UserState']==""? "Choose State":$_SESSION['userInfo']['UserState']; echo $state; ?></option>
 	     	<option>Philippines</option>
 	     	<option>Singapore</option>
 	      <option>Korea</option>
@@ -54,7 +54,7 @@
 	<div class="form-group">
 	    <label>Country</label>
 	    <select class="form-control">
-	    	<option>Choose Country</option>
+	    	<option><?php $country = $_SESSION['userInfo']['UserCountry']==""? "Choose Country":$_SESSION['userInfo']['UserCountry']; echo $country; ?></option>
 	     	<option>Philippines</option>
 	     	<option>Singapore</option>
 	      <option>Korea</option>
@@ -62,11 +62,11 @@
   	</div>
 	  <div class="form-group">
 	    <label>Primary Address</label>
-	    <input type="text" class="form-control">
+	    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['userInfo']['UserAddress'] ?>" disabled>
 	  </div>
 	  <div class="form-group">
 	    <label>Secondary Address</label>
-	    <input type="text" class="form-control">
+	    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['userInfo']['UserAddress2'] ?>" disabled>
 	  </div>
 
 	  <button type="button" class="btn pull-right">DONE</button>
--- lib/fetchinfo.php
+++ lib/fetchinfo.php
@@ -30,5 +30,4 @@ class FetchInfo extends CARTON
 }
 
 
-
 ?>

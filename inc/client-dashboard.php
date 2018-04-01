32c8e1ff548855f70138bce6f1838f0b42542de2
Jericho Gonzales <32585292+jerkjerkss@users.noreply.github.com>
2018-04-01 12:13:18 +0800

    dashboard update

--- inc/client-dashboard.php
+++ inc/client-dashboard.php
@@ -1,13 +1,118 @@
-<center>
-	<h1 class="heading">CartOn Dashboard</h1><br>
 
+<!-- <center>
+	<h1 class="heading">CartOn Dashboard</h1><br>
     <div class="quick-actions_homepage">
       <ul class="quick-actions">
             <button class="custom-btn btn-6"><span>My Products</span></button>
-           <button class="custom-btn btn-6"><span>Account Settings</span></button>
-            <button class="custom-btn btn-6"><span>Manage Orders</span></button>
+           <button class="custom-btn btn-6"><span>Settings</span></button>
+            <button class="custom-btn btn-6"><span> Manage Orders</span></button>
             <button class="custom-btn btn-6"><span>Manage Workers</span></button>
       </ul>
     </div>
-
 </center>
+ -->
+  	<!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+  	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link href="css/helper-dashboard.css" rel="stylesheet">
+    <link href="css/style-dashboard.css" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+     <script src="js/raphael-min.js"></script>
+    <script src="js/morris.js"></script>
+    <script src="js/dashboard1-init.js"></script>
+    <script src="js/scripts.js"></script>
+<div class="container"><br><br>
+<body class="fix-header fix-sidebar">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card p-30">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-shopping-cart f-s-40 color-primary"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <h3>Manage Products</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card p-30">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-cog f-s-40 color-success"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <h3>Account Settings</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card p-30">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-archive f-s-40 color-warning"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <h3>Manage Orders</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card p-30">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-user f-s-40 color-danger"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <h3>Manage Workers</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row bg-white m-l-0 m-r-0 box-shadow ">
+                    <div class="col-lg-8">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Extra Area Chart</h4>
+                                <div id="extra-area-chart"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card">
+                        	<h4 class="card-title">Chart</h4>
+                            <div class="card-body browser" style="margin-top: 45px;">
+                                <p class="f-w-600">iMacs<span class="pull-right">85%</span></p>
+                                <div class="progress ">
+                                    <div role="progressbar" style="width: 85%; height:8px;" class="progress-bar bg-danger wow animated progress-animated"> <span class="sr-only">60% Complete</span> </div>
+                                </div>
+
+                                <p class="m-t-30 f-w-600">iBooks<span class="pull-right";">90%</span></p>
+                                <div class="progress">
+                                    <div role="progressbar" style="width: 90%; height:8px;" class="progress-bar bg-info wow animated progress-animated"> <span class="sr-only">60% Complete</span> </div>
+                                </div>
+
+                                <p class="m-t-30 f-w-600">iPhone<span class="pull-right">65%</span></p>
+                                <div class="progress">
+                                    <div role="progressbar" style="width: 65%; height:8px;" class="progress-bar bg-success wow animated progress-animated"> <span class="sr-only">60% Complete</span> </div>
+                                </div>
+
+                                <p class="m-t-30 f-w-600">Samsung<span class="pull-right">65%</span></p>
+                                <div class="progress">
+                                    <div role="progressbar" style="width: 65%; height:8px;" class="progress-bar bg-warning wow animated progress-animated"> <span class="sr-only">60% Complete</span> </div>
+                                </div>
+
+								<p class="m-t-30 f-w-600">android<span class="pull-right">65%</span></p>
+                                <div class="progress m-b-30">
+                                    <div role="progressbar" style="width: 65%; height:8px;" class="progress-bar bg-success wow animated progress-animated"> <span class="sr-only">60% Complete</span> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
